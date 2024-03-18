f6d67ea3cd35e33e242d0b57d16d3238467dcf77
MateuszAn123 <mateuszanuszewski11@gmail.com>
2024-03-18 17:10:45 +0100

    pociag + - / *

--- PROJECT_DATA_STRUCTURES_CALCULATOR/PROJECT_DATA_STRUCTURES_CALCULATOR/PROJECT_DATA_STRUCTURES_CALCULATOR.cpp
+++ PROJECT_DATA_STRUCTURES_CALCULATOR/PROJECT_DATA_STRUCTURES_CALCULATOR/PROJECT_DATA_STRUCTURES_CALCULATOR.cpp
@@ -16,50 +16,60 @@ public:
 	operatorType operator_type;
 	int n_numbers;
 	int index;
-	MyOperator(char txt[3]): n_numbers(0),index(0){
-		if(compare_char(txt, "MAX",3) == 1){
+	MyOperator(char txt[3]): n_numbers(0),index(1){
+		if(compare_char(txt, "MAX",3) == 1)
+		{
 			operator_type = MAX;
+			index = 2;
+
 		}else if (compare_char(txt, "MIN",3) == 1)
 		{
 			operator_type = MIN;
+			index = 2;
 		}else if (compare_char(txt, "*",1) == 1)
 		{
 			operator_type = MUL;
 			n_numbers = 2;
+			index = 3;
 		}else if (compare_char(txt, "+",1) == 1)
 		{
 			operator_type = ADD;
 			n_numbers = 2;
+			index = 4;
 		}else if (compare_char(txt, "-",1) == 1)
 		{
 			operator_type = SUB;
 			n_numbers = 2;
+			index = 4;
 		}else if (compare_char(txt, "/",1) == 1)
 		{
 			operator_type = DIV;
 			n_numbers = 2;
+			index = 3;
 		}
 		else if (compare_char(txt, "IF",2) == 1)
 		{
 			operator_type = IF;
 			n_numbers = 3;
+			index = 2;
 		}
 		else if (compare_char(txt, "N", 1) == 1)
 		{
 			operator_type = N;
 			n_numbers = 1;
+			index = 2;
 		}
 		else if (compare_char(txt, "(", 1) == 1)
 		{
 			operator_type = OPEN;
+			index = 1;
 		}
 		else if (compare_char(txt, ")", 1) == 1)
 		{
 			operator_type = CLOSE;
+			index=1;
 		}
-		else{
-			cout << "nie wykryto zadnego operatora\n";
-		}
+
 		//cout << operator_type << endl;
 	}
 	bool compare_char(char* a, char* b, int length)
@@ -110,17 +120,20 @@ case CLOSE:
 }	
 
 
-class Token{
-public:
-	int number = 0;
-	MyOperator *_operator = nullptr;
-	bool isNumber(char token)
+bool isNumber(char token)
 	{
 		if (token <= '9' && token >= '0')
 			return true;
 		else
 			return false;
 	}
+
+
+class Token{
+public:
+	int number = 0;
+	MyOperator *_operator = nullptr;
+	
 
 	Token(char txt[MAX_INT_LENGTH]){
 		if (isNumber(txt[0])){
@@ -167,8 +180,8 @@ template <typename T> class LinkedList {
 private:
 	Node<T> *head;
 	Node<T> *tail;
-	int number_elements = 0;
 public:
+	int number_elements = 0;
 	LinkedList() : head(nullptr), tail(nullptr){
 	}
 	void append(const T &newData) {
@@ -220,7 +233,6 @@ public:
 	}
 	void Print_last_to_first() {
 		Node<T>* temp = tail;
-		cout << "Numer of elements list : " << number_elements << endl;
 		while (temp != nullptr)
 		{
 			cout << temp->data << " ";
@@ -230,7 +242,6 @@ public:
 	}
 	void Print_first_to_last() {
 		Node<T>* temp = head;
-		cout << "Numer of elements list : " << number_elements << endl;
 		while (temp != nullptr)
 		{
 			cout << temp->data << " ";
@@ -253,34 +264,85 @@ public:
         return tail->data;
     }
 
+	bool isEmpty() const {
+        return (head == nullptr);
+    }
 };
 
-
+void operation_on_stack_and_list(LinkedList list, LinkedList stack, Token *t)
+{
+if(stack.isEmpty())
+	{
+	stack.append(*t);
+	}
+	else if ((stack.get_last()._operator->n_numbers<= list.number_elements) && ( t->_operator->index >= stack.get_last()._operator->index))
+	{
+		list.append(stack.get_last());
+		stack.remove_last();
+		stack.append(*t);
+	}
+	else {			
+		stack.append(*t);
+	}	
+}
 
 
 int main() {
 
 	LinkedList<Token> list;
-
+	LinkedList<Token> stack;
+	bool ifPara = false;
 
 	int how_many ;
 	cin>>how_many;
 	char input[9];
-
-	for (int i = 0; i < how_many; i++)
+	int i=0;
+	while(i < how_many)
 	{
 		cin >> input;
-		Token *t = new Token(input);
-		list.append(*t);
-	}
+		if(input[0]=='.')
+		{
+			while(!stack.isEmpty())
+			{
+				list.append(stack.get_last());
+				stack.remove_last();
+			}
+			i++;
+			break;
+		}	
+		else if(isNumber(input[0]))
+		{
+			Token *t = new Token(input);
+			list.append(*t);
+		}
+		else{
+			Token *t = new Token(input);
+			if(input = "(")
+			{
 
-	list.Print_last_to_first();
+				ifPara = true;
+				stack.append(*t);
+			}
+			else 
+			{
+			if(stack.isEmpty())
+				{
+				stack.append(*t);
+				}
+			else if ((stack.get_last()._operator->n_numbers<= list.number_elements) && ( t->_operator->index >= stack.get_last()._operator->index))
+				{
+				list.append(stack.get_last());
+				stack.remove_last();
+				stack.append(*t);
+				}
+			else {			
+				stack.append(*t);
+			}	
+			}
+			
+		}
+	}
 	list.Print_first_to_last();
-	cout<<endl;
-	cout<<"Pierwszy element "<<list.get_last()<<endl;
-	list.remove_last();
-	cout<<"Pierwszy element "<<list.get_last()<<endl;
-	list.Print_last_to_first();
-	list.Print_first_to_last();
+
 	return 0;
 }
